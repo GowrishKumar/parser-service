e172053ac6bd96a2ffb09ad71a1820556a909c1c
Gowrishkumar Jagadeeswaran <gowrishkumar@live.com>
2020-10-08 21:35:27 -0400

    added simple logging middleware

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,16 @@ import express from "express";
 
 const app = express();
 
+function loggerMiddleware(request: express.Request, response: express.Response, next:any){
+    console.log(`${new Date()} ${request.method} ${request.path}`);
+    next();
+}
+app.use(loggerMiddleware);
+
 app.get('/', (request, response) => {
     response.send("Hello Parser");
 });
 
-app.listen(5000);
+app.listen(5000, ()=> {
+    console.log('Server listening on port 5000');
+});
