ecd99a99ec0b74c00d8d75738439d8487ddb4e63
Gowrishkumar Jagadeeswaran <gowrishkumar@live.com>
2020-10-09 02:22:07 -0400

    added simple unit test with mocha and chai

---
+++ tests/services/services.test.ts
@@ -0,0 +1,17 @@
+import {describe} from "mocha";
+import {assert} from "chai";
+import {ParserService} from "../../src/services/parser.service";
+import {ClientModel} from "../../src/models/client.model";
+
+describe('Parser Service', () => {
+    it('should parse', () => {
+        const parserService = new ParserService();
+        const result = parserService.parse("JHON0000MICHAEL0009994567");
+        const expected: ClientModel = {
+            firstName: "JHON0000",
+            lastName:"MICHAEL000",
+            clientId:"9994567"
+        }
+        assert.deepEqual(result, expected);
+    })
+})
