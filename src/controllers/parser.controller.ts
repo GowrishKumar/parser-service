1908f03277901d85e3a099613f25d004c0abe243
Gowrishkumar Jagadeeswaran <gowrishkumar@live.com>
2020-10-09 03:20:53 -0400

    updated validation to check if client id is a valid number

--- src/controllers/parser.controller.ts
+++ src/controllers/parser.controller.ts
@@ -3,7 +3,8 @@ import {check } from "express-validator"
 import {Validator} from "../milddlewares/validator.middleware";
 import {ParserService} from "../services/parser.service";
 const validations = [
-    check('data').not().isEmpty().isLength({min:25, max:25}).withMessage('data must be a 25 character string')
+    check('data').not().isEmpty().isLength({min:25, max:25}).withMessage('data must be a 25 character string'),
+    check('data').custom(value => { return !isNaN(value) && !isNaN(parseFloat(value))}).withMessage("last 7 chats should be a number")
 ]
 export class ParserController {
     public path = '/parse';
