f489b3260c077d50bba044e7a83398ce53f54666
Gowrishkumar Jagadeeswaran <gowrishkumar@live.com>
2020-10-09 09:04:44 -0400

    enforced signature thru interfaces

--- src/controllers/parser.controller.ts
+++ src/controllers/parser.controller.ts
@@ -2,13 +2,14 @@ import express from "express";
 import {check } from "express-validator"
 import {Validator} from "../milddlewares/validator.middleware";
 import {ParserService} from "../services/parser.service";
+import {IController} from "../models/controller.model";
 const validations = [
     check('data').not().isEmpty().isLength({min:25, max:25}).withMessage('data must be a 25 character string'),
     check('data').custom(value => { let id = value.substr(value.length -7); return !isNaN(id) && !isNaN(parseFloat(id))}).withMessage("last 7 chats should be a number")
 ]
-export class ParserController {
-    public path = '/v1/parse';
-    public pathV2 = '/v2/parse';
+export class ParserController implements IController {
+    private path = '/v1/parse';
+    private pathV2 = '/v2/parse';
     public router = express.Router();
 
     constructor(private parserService: ParserService) {
